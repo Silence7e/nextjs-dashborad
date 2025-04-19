1ddbc792fb5895a9d1799a6dd823d2f2794239c3
robin <robin@woo.network>
2025-04-19 10:56:11 +0800

    metadata sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,32 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Course Dashboard, built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+  openGraph: {
+    description:
+      "The official Next.js Course Dashboard, built with App Router.",
+    // 可以添加其他 Open Graph 属性
+    title: "Acme Dashboard",
+    type: "website",
+    images: [
+      {
+        url: "/og-image.jpg", // 相对路径，会自动与 metadataBase 组合
+        width: 1200,
+        height: 630,
+        alt: "Acme Dashboard",
+      },
+    ],
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
